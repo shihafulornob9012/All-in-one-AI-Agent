c494c8d49196776b32f95650b1401d369579b9ee
shihafulornob9012 <shihaful2254901012@student.bup.edu.bd>
2025-11-29 22:30:07 +0600

    2.0

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <div className='fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32'>
-        <img src={assets.AI} alt="" className='w-32 sm:w-44' onClick={()=> navigate('/')}/>
+        <img src={assets.logo} alt="" className='w-320 sm:w-80' onClick={()=> navigate('/')}/>
         {
             user ? <UserButton /> :(
                 <button onClick={openSignIn} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>Get Started <ArrowRight className='w-4 h-4'/> </button>
